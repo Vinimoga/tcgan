102a49fbb62b86d7ceb123abe9921525b49c6a75
Vinimoga <vini.motta.garcia@gmail.com>
2025-01-16 18:33:54 +0000

    Add initial TCGAN model minerva-like and add training original test notebook

--- Minerva_experiments/TCGAN_model.py
+++ Minerva_experiments/TCGAN_model.py
@@ -1,31 +1,73 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
 
 class TCGAN_Discriminator(nn.Module):
-    def __init__(self, input_shape, units, kernel_size, strides, layer_steps, device):
+    def __init__(
+        self, 
+        input_shape = (60,6), 
+        noise_shape = 100, 
+        n_layers = 4, 
+        kernel_size = 10, 
+        strides = 2, 
+        g_units_base = 32,
+        device = 'cpu' 
+    ):
+        """The TCGAN discriminator is designed to classify time series data using a 
+        convolutional neural network (CNN) architecture. It takes a time series input 
+        and outputs a classification score.
+        Parameters
+        ----------
+        input_shape : tuple, optional
+            Shape of the input time series data, by default (60, 6)
+        noise_shape : int, optional
+            Shape of the noise vector, by default 100
+        n_layer : int, optional
+            Number of convolutional layers, by default 4
+        kernel_size : int, optional
+            Size of the convolutional kernel, by default 10
+        strides : int, optional
+            Stride size for the convolutional layers, by default 2
+        g_units_base : int, optional
+            Base number of units for the generator, by default 32
+        device : str, optional
+            Device to run the model on, by default 'cpu'
+        """        
         super(TCGAN_Discriminator, self).__init__()
+        self.input_shape = input_shape
+        self.noise_shape = noise_shape
+        self.n_layers = n_layers
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.g_units_base = g_units_base
+        self.device = device
+
+        self.calculate_constants()
+
         self.l1 = nn.Conv1d(
-            input_shape[-1],
-            units[0],
-            kernel_size,
-            stride=strides,
-            padding=6,
-            device=device,
+            self.input_shape[-1], self.units[0], self.kernel_size, stride=self.strides, padding=6, device=self.device
         )
         self.l2 = nn.Conv1d(
-            units[0], units[1], kernel_size, stride=strides, padding=4, device=device
+            self.units[0], self.units[1], self.kernel_size, stride=self.strides, padding=4, device=self.device
         )
         self.l3 = nn.Conv1d(
-            units[1], units[2], kernel_size, stride=strides, padding=4, device=device
+            self.units[1], self.units[2], self.kernel_size, stride=self.strides, padding=4, device=self.device
         )
-        self.l4 = nn.BatchNorm1d(units[2], device=device)
+        self.l4 = nn.BatchNorm1d(
+            self.units[2], device=self.device
+        )
         self.l5 = nn.Conv1d(
-            units[2], units[3], kernel_size, stride=strides, padding=4, device=device
+            self.units[2], self.units[3], self.kernel_size, stride=self.strides, padding=4, device=self.device
         )
-        self.l6 = nn.BatchNorm1d(units[3], device=device)
-        self.l7 = nn.Linear(layer_steps[0] * units[-1], 1, device=device)
+        self.l6 = nn.BatchNorm1d(
+            self.units[3], device=self.device
+        )
+        self.l7 = nn.Linear(
+            self.layer_steps[0] * self.units[-1], 1, device=self.device
+        )
+
+        self.initweights()
 
     def forward(self, x):
         x = self.l1(x)
@@ -42,56 +84,109 @@ class TCGAN_Discriminator(nn.Module):
         x = torch.flatten(x, start_dim=1)
         x = self.l7(x)
         return x
+    
+    def calculate_constants(self):
+        layer_steps = [self.input_shape[0]]
+        for i in range(self.n_layers):
+            layer_steps.append(int(np.ceil(float(layer_steps[-1]) / float(self.strides))))
+        layer_steps.reverse()
+        self.layer_steps = layer_steps
+
+        conv_units = []
+        if self.n_layers > 1:
+            conv_units.append(self.g_units_base)
+            for _ in range(self.n_layers - 2):  # minus the first and the last layers
+                conv_units.append(conv_units[-1] * 2)
+        conv_units.reverse()
+        # the last layer must be aligned to the number of dimensions of input.
+        conv_units.append(self.input_shape[-1])
+        self.conv_units = conv_units
+
+        units = [32]
+        for _ in range(self.n_layers - 1):  # exclude the first layer.
+            units.append(units[-1] * 2)
+        self.units = units
+
+
+    def initweights(self):
+            self.l1.weight = nn.init.trunc_normal_(self.l1.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l1.bias.data.zero_()
+            self.l2.weight = nn.init.trunc_normal_(self.l2.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l2.bias.data.zero_()
+            self.l3.weight = nn.init.trunc_normal_(self.l3.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l3.bias.data.zero_()
+            self.l4.weight = nn.init.trunc_normal_(self.l4.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l4.bias.data.zero_()
+            self.l5.weight = nn.init.trunc_normal_(self.l5.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l5.bias.data.zero_()
+            self.l6.weight = nn.init.trunc_normal_(self.l6.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l6.bias.data.zero_()
+            self.l7.weight = nn.init.trunc_normal_(self.l7.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l7.bias.data.zero_()
+            '''for m in self.modules():
+                if isinstance(m, nn.Conv1d):
+                    m.weight= nn.init.trunc_normal_(m.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+                    m.bias.data.zero_()
+                elif isinstance(m, nn.Linear):
+                    m.weight= nn.init.trunc_normal_(m.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+                    m.bias.data.zero_()
+                elif isinstance(m, nn.BatchNorm1d):
+                    m.weight= nn.init.trunc_normal_(m.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+                    m.bias.data.zero_()
+            '''
 
 
 class TCGAN_Generator(nn.Module):
     def __init__(
-        self, input_layer, conv_units, kernel_size, strides, layer_steps, device
+        self, 
+        input_shape = (60,6),
+        noise_shape = 100, 
+        n_layers = 4, 
+        kernel_size = 10, 
+        strides = 2, 
+        g_units_base = 32, 
+        device = 'cpu'
     ):
         super(TCGAN_Generator, self).__init__()
-        self.layer_steps = layer_steps
-        self.conv_units = conv_units
+        self.input_shape = input_shape
+        self.noise_shape = noise_shape
+        self.n_layers = n_layers
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.g_units_base = g_units_base
+        self.device = device
+
+        self.calculate_constants()
 
         self.l1 = nn.Linear(
-            input_layer, layer_steps[0] * conv_units[0] * 2, device=device
+            noise_shape, self.layer_steps[0] * self.conv_units[0] * 2, device=self.device
         )
-        self.l2 = nn.BatchNorm1d(layer_steps[0] * conv_units[0] * 2, device=device)
+        self.l2 = nn.BatchNorm1d(
+            self.layer_steps[0] * self.conv_units[0] * 2, device=self.device
+        )
         self.l3 = nn.ConvTranspose1d(
-            conv_units[0] * 2,
-            conv_units[0],
-            kernel_size,
-            stride=strides,
-            padding=4,
-            device=device,
+            self.conv_units[0] * 2, self.conv_units[0], self.kernel_size, stride=self.strides, padding=4, device=self.device
         )
-        self.l4 = nn.BatchNorm1d(conv_units[0], device=device)
+        self.l4 = nn.BatchNorm1d(
+            self.conv_units[0], device=device
+        )
         self.l5 = nn.ConvTranspose1d(
-            conv_units[0],
-            conv_units[1],
-            kernel_size,
-            stride=strides,
-            padding=4,
-            device=device,
+            self.conv_units[0], self.conv_units[1], self.kernel_size, stride=self.strides, padding=4, device=self.device
         )
-        self.l6 = nn.BatchNorm1d(conv_units[1], device=device)
+        self.l6 = nn.BatchNorm1d(
+            self.conv_units[1], device=device
+        )
         self.l7 = nn.ConvTranspose1d(
-            conv_units[1],
-            conv_units[2],
-            kernel_size,
-            stride=strides,
-            padding=4,
-            device=device,
+            self.conv_units[1], self.conv_units[2], self.kernel_size, stride=self.strides, padding=4, device=self.device
         )
-        self.l8 = nn.BatchNorm1d(conv_units[2], device=device)
+        self.l8 = nn.BatchNorm1d(
+            self.conv_units[2], device=self.device
+        )
         self.l9 = nn.ConvTranspose1d(
-            conv_units[2],
-            conv_units[3],
-            kernel_size,
-            stride=strides,
-            padding=6,
-            output_padding=0,
-            device=device,
+            self.conv_units[2], self.conv_units[3], self.kernel_size, stride=self.strides, padding=6, output_padding=0, device=self.device 
         )
+        
+        self.initweights()
 
     def forward(self, x):
         x = self.l1(x)
@@ -111,28 +206,113 @@ class TCGAN_Generator(nn.Module):
         x = self.l9(x)
         return x
 
+    def calculate_constants(self):
+        steps = self.input_shape[0]
+        layer_steps = [steps]
+        for i in range(self.n_layers):
+            layer_steps.append(int(np.ceil(float(layer_steps[-1]) / float(self.strides))))
+        layer_steps.reverse()
+        self.layer_steps = layer_steps
+
+        conv_units = []
+        if self.n_layers > 1:
+            conv_units.append(self.g_units_base)
+            for _ in range(self.n_layers - 2):  # minus the first and the last layers
+                conv_units.append(conv_units[-1] * 2)
+        conv_units.reverse()
+        # the last layer must be aligned to the number of dimensions of input.
+        conv_units.append(self.input_shape[-1])
+        self.conv_units = conv_units
+
+        units = [32]
+        for _ in range(self.n_layers - 1):  # exclude the first layer.
+            units.append(units[-1] * 2)
+        self.units = units
+
+
+    def initweights(self):
+            self.l1.weight = nn.init.trunc_normal_(self.l1.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l1.bias.data.zero_()
+            self.l2.weight = nn.init.trunc_normal_(self.l2.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l2.bias.data.zero_()
+            self.l3.weight = nn.init.trunc_normal_(self.l3.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l3.bias.data.zero_()
+            self.l4.weight = nn.init.trunc_normal_(self.l4.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l4.bias.data.zero_()
+            self.l5.weight = nn.init.trunc_normal_(self.l5.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l5.bias.data.zero_()
+            self.l6.weight = nn.init.trunc_normal_(self.l6.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l6.bias.data.zero_()
+            self.l7.weight = nn.init.trunc_normal_(self.l7.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l7.bias.data.zero_()
+            self.l8.weight = nn.init.trunc_normal_(self.l8.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l8.bias.data.zero_()
+            self.l9.weight = nn.init.trunc_normal_(self.l9.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l9.bias.data.zero_()
+
+
 class TCGAN_Encoder(nn.Module):
-    def __init__(self, input_shape, units, kernel_size, strides, device):
-        super(TCGAN_Discriminator, self).__init__()
+    def __init__(
+        self, 
+        input_shape = (60,6), 
+        noise_shape = 100, 
+        n_layer = 4, 
+        kernel_size = 10, 
+        strides = 2, 
+        g_units_base = 32,
+        device = 'cpu' 
+    ):
+        """The TCGAN discriminator is designed to classify time series data using a 
+        convolutional neural network (CNN) architecture. It takes a time series input 
+        and outputs a classification score.
+        Parameters
+        ----------
+        input_shape : tuple, optional
+            Shape of the input time series data, by default (60, 6)
+        noise_shape : int, optional
+            Shape of the noise vector, by default 100
+        n_layer : int, optional
+            Number of convolutional layers, by default 4
+        kernel_size : int, optional
+            Size of the convolutional kernel, by default 10
+        strides : int, optional
+            Stride size for the convolutional layers, by default 2
+        g_units_base : int, optional
+            Base number of units for the generator, by default 32
+        device : str, optional
+            Device to run the model on, by default 'cpu'
+        """        
+        super(TCGAN_Encoder, self).__init__()
+        self.input_shape = input_shape
+        self.noise_shape = noise_shape
+        self.n_layer = n_layer
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.g_units_base = g_units_base
+        self.device = device
+
+        self.calculate_constants()
+
         self.l1 = nn.Conv1d(
-            input_shape[-1],
-            units[0],
-            kernel_size,
-            stride=strides,
-            padding=6,
-            device=device,
+            input_shape[-1], self.units[0], kernel_size, stride=strides, padding=6, device=device
         )
         self.l2 = nn.Conv1d(
-            units[0], units[1], kernel_size, stride=strides, padding=4, device=device
+            self.units[0], self.units[1], kernel_size, stride=strides, padding=4, device=device
         )
         self.l3 = nn.Conv1d(
-            units[1], units[2], kernel_size, stride=strides, padding=4, device=device
+            self.units[1], self.units[2], kernel_size, stride=strides, padding=4, device=device
         )
-        self.l4 = nn.BatchNorm1d(units[2], device=device)
+        self.l4 = nn.BatchNorm1d(
+            self.units[2], device=device
+        )
         self.l5 = nn.Conv1d(
-            units[2], units[3], kernel_size, stride=strides, padding=4, device=device
+            self.units[2], self.units[3], kernel_size, stride=strides, padding=4, device=device
         )
-        self.l6 = nn.BatchNorm1d(units[3], device=device)
+        self.l6 = nn.BatchNorm1d(
+            self.units[3], device=device
+        )
+
+        self.initweights()
 
     def forward(self, x):
         x = self.l1(x)
@@ -147,3 +327,41 @@ class TCGAN_Encoder(nn.Module):
         x = F.leaky_relu(x, 0.2)
         x = torch.flatten(x, start_dim=1)
         return x
+    
+    def calculate_constants(self):
+        steps = self.input_shape[0]
+        layer_steps = [steps]
+        for i in range(self.n_layers):
+            layer_steps.append(int(np.ceil(float(layer_steps[-1]) / float(self.strides))))
+        layer_steps.reverse()
+        self.layer_steps = layer_steps
+
+        conv_units = []
+        if self.n_layers > 1:
+            conv_units.append(self.g_units_base)
+            for _ in range(self.n_layers - 2):  # minus the first and the last layers
+                conv_units.append(conv_units[-1] * 2)
+        conv_units.reverse()
+        # the last layer must be aligned to the number of dimensions of input.
+        conv_units.append(self.input_shape[-1])
+        self.conv_units = conv_units
+
+        units = [32]
+        for _ in range(self.n_layers - 1):  # exclude the first layer.
+            units.append(units[-1] * 2)
+        self.units = units
+
+
+    def initweights(self):
+            self.l1.weight = nn.init.trunc_normal_(self.l1.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l1.bias.data.zero_()
+            self.l2.weight = nn.init.trunc_normal_(self.l2.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l2.bias.data.zero_()
+            self.l3.weight = nn.init.trunc_normal_(self.l3.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l3.bias.data.zero_()
+            self.l4.weight = nn.init.trunc_normal_(self.l4.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l4.bias.data.zero_()
+            self.l5.weight = nn.init.trunc_normal_(self.l5.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l5.bias.data.zero_()
+            self.l6.weight = nn.init.trunc_normal_(self.l6.weight, mean=0.0, std=0.02, a=-0.04, b=0.04)
+            self.l6.bias.data.zero_()
